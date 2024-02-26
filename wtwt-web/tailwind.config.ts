2b5d9bdcb0f5d13d758899a78aec32e52618d8a8
yui880 <kimyuna521@gmail.com>
2024-02-26 17:58:01 +0900

    style: 화면 크기, 기본 색상 설정

--- wtwt-web/tailwind.config.ts
+++ wtwt-web/tailwind.config.ts
@@ -1,17 +1,36 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      maxWidth: {
+        400: '400px',
+      },
+      colors: {
+        primary: {
+          main: '#3C70FF',
+          subMain: '#6A92FF',
+          background: '#F8F8F9',
+        },
+        text: {
+          general: '#1E1E1E',
+          inputLabel: '#707070',
+          title: '#8F8F8F',
+          black: '#000000',
+          placeHolder: '#C1C1C1',
+        },
+        grey: {
+          grey1: '#F8F8F9',
+        },
+      },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
