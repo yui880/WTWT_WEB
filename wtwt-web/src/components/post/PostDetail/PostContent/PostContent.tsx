2f1a23f86d084954bf8b33d306e1fdc3bef217e5
yui880 <kimyuna521@gmail.com>
2024-03-19 22:10:23 +0900

    feat: 게시글 내 이미지 클릭시 크게 보는 기능 구현

--- wtwt-web/src/components/post/PostDetail/PostContent/PostContent.tsx
+++ wtwt-web/src/components/post/PostDetail/PostContent/PostContent.tsx
@@ -1,5 +1,9 @@
 import { type ImageType } from '@component/types/image';
 import Image from 'next/image';
+import { useModal } from '@component/hooks/useModal';
+import { Simulate } from 'react-dom/test-utils';
+import click = Simulate.click;
+import { useState } from 'react';
 
 interface PostContentProps {
   content: string;
@@ -7,23 +11,46 @@ interface PostContentProps {
 }
 
 export const PostContent = ({ content, images }: PostContentProps) => {
+  const { Modal, closeModal, openModal, isOpen } = useModal();
+  const [clickedImage, setClickedImage] = useState('');
+
+  const clickImageHandler = (uri: string) => {
+    setClickedImage(uri);
+    openModal();
+  };
+
   return (
-    <div className="flex flex-col gap-6 px-6 py-8">
-      <div className="text-sm">{content}</div>
-      <div className="flex flex-row gap-3">
-        {images.map(({ uri, name }, i) => {
-          return (
-            <Image
-              className="rounded-xl"
-              key={name + i}
-              src={uri}
-              alt={name}
-              height={120}
-              width={120}
-            />
-          );
-        })}
+    <>
+      <div className="flex flex-col gap-6 px-6 py-8">
+        <div className="text-sm">{content}</div>
+        <div className="flex flex-row gap-3">
+          {images.map(({ uri, name }, i) => {
+            return (
+              <Image
+                className="rounded-xl"
+                key={name + i}
+                src={uri}
+                alt={name}
+                height={120}
+                width={120}
+                onClick={() => {
+                  clickImageHandler(uri);
+                }}
+              />
+            );
+          })}
+        </div>
       </div>
-    </div>
+      <Modal isOpen={isOpen} closeModal={closeModal} fastSpeed={true}>
+        <div>
+          <Image
+            src={clickedImage}
+            alt="largeImage"
+            height={window.innerWidth}
+            width={window.innerWidth}
+          />
+        </div>
+      </Modal>
+    </>
   );
 };
