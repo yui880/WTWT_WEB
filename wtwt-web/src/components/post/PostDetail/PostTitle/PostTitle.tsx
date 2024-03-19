4d67c7466aa381575cde0d710ad44bffc4a5f902
yui880 <kimyuna521@gmail.com>
2024-03-19 17:35:07 +0900

    feat: PostTitle에 카테고리 출력 추가
    어떤 카테고리(여행지)인지 출력하는 부분 추가

--- wtwt-web/src/components/post/PostDetail/PostTitle/PostTitle.tsx
+++ wtwt-web/src/components/post/PostDetail/PostTitle/PostTitle.tsx
@@ -1,17 +1,24 @@
 import { type MemberType } from '@component/types/user';
 import { IoEyeSharp } from 'react-icons/io5';
+import { HiFlag } from 'react-icons/hi';
 import Image from 'next/image';
+import { type CategoryType } from '@component/types/post';
 
 interface PostTitleProps {
   title: string;
   postDate: Date;
   hits: number;
   writer: MemberType;
+  category: CategoryType;
 }
-export const PostTitle = ({ title, postDate, hits, writer }: PostTitleProps) => {
+export const PostTitle = ({ title, postDate, hits, writer, category }: PostTitleProps) => {
   return (
     <div className="flex flex-col gap-4 px-6 py-3">
       <div className="flex flex-col gap-1">
+        <div className="flex flex-row gap-1 pb-1">
+          <HiFlag className="text-primary-main" />
+          <div className="text-xs text-gray-600">{category.name}</div>
+        </div>
         <div className="text-lg font-semibold">{title}</div>
         <div className="flex flex-row items-center gap-6 text-xs font-light text-gray-400">
           <div>{`${postDate.getFullYear()}. ${postDate.getMonth() + 1}. ${postDate.getDay()}`}</div>
