22f7aea743682380f3d45b32c553a26f77a34f19
yui880 <kimyuna521@gmail.com>
2024-03-28 23:03:30 +0900

    feat: MainHeader 컴포넌트 구현
    홈 화면(메인 화면)에서 사용되는 헤더 구현

---
+++ wtwt-web/src/components/home/Home.tsx
@@ -0,0 +1,10 @@
+import MainHeader from '@component/components/common/Header/MainHeader';
+import { Container } from '@component/components/common/Container';
+
+export const Home = () => {
+  return (
+    <Container>
+      <MainHeader />
+    </Container>
+  );
+};
